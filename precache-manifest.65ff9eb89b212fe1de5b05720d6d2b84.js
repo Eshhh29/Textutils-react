38213b1c151109605d1366d9eba7e93717992cab
Eshhh29 <111509455+Eshhh29@users.noreply.github.com>
2022-08-18 18:16:55 +0530

    Updates

---
+++ precache-manifest.65ff9eb89b212fe1de5b05720d6d2b84.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "0a04319e91d0e3b42601",
+    "url": "/textutils/static/css/main.3aa5302d.chunk.css"
+  },
+  {
+    "revision": "0a04319e91d0e3b42601",
+    "url": "/textutils/static/js/main.0a04319e.chunk.js"
+  },
+  {
+    "revision": "03ae7ea834bbe7645a20",
+    "url": "/textutils/static/js/1.03ae7ea8.chunk.js"
+  },
+  {
+    "revision": "946d8365c0317c9ba6aa",
+    "url": "/textutils/static/js/2.946d8365.chunk.js"
+  },
+  {
+    "revision": "8c5d3a7ccc996c2dc493",
+    "url": "/textutils/static/js/runtime~main.8c5d3a7c.js"
+  },
+  {
+    "revision": "df9440eb2fbaf49733992c3caab65241",
+    "url": "/textutils/index.html"
+  }
+];
